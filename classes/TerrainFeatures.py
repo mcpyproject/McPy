f6f443d68b8f505b26b73933f77fee284556a5e1
Geolykt <admin@geolykt.de>
2020-08-05 22:21:09 +0200

    Trees now respect air
    Well, I do not like the way it is made, but eh

--- classes/TerrainFeatures.py
+++ classes/TerrainFeatures.py
@@ -21,6 +21,18 @@ def _generate_block(chunk, chunkpos: [int, int, int], material: str):
         #this is where we should load the correct chunk
         # however right now that is Not Possible
     _generate_block_unsafely(chunk, chunkpos, [chunk.xPos * 16 + chunkpos[0], chunk.yPos * 16 + chunkpos [1], chunk.zPos * 16 + chunkpos [2]], material)
+
+def _is_air (chunk, x: int, y: int, z: int):
+    """ Checks If a Block at a given position relative to the given chunk is air """
+    try:
+        var: str = chunk.blocks["{},{},{}".format(x, y, z)]
+        if var is None:
+            return True
+        return bool(var in ("AIR", "CAVE_AIR", "VOID_AIR"))
+    except:
+        return True
+    pass
+
 class AbstractTerrainFeature:
     
     def generation_attempt(self, random: float, chunk, chunk_x: int, chunk_y: int, chunk_z: int, is_top_layer: bool):
@@ -99,16 +111,17 @@ class AbstractTreeGenerator(AbstractTerrainFeature):
             pass
         elif random < self.chance and chunk_y < self.max_y and chunk_y > self.min_y:
             height: int = randint(self.min_y, self.max_y)
-            block_x: int = chunk_x + chunk.xPos
-            block_z: int = chunk_z + chunk.zPos
+            for delta_y in range(height):#check If can generate
+                if _is_air(chunk, chunk_x, chunk_y + delta_y, chunk_z):
+                    return False
             for delta_y in range(height):#trunk generation
-                # Danger! Statement below may be victim of misunderstandings
-                block_y: int = delta_y + chunk_y + chunk.yPos
                 # TODO trunk orientation
                 _generate_block(chunk, [chunk_x, chunk_y + delta_y, chunk_z], self.trunk_name)
                 
             blocks = self._leaves()
             for b in blocks:
+                if _is_air(chunk, chunk_x + b[0], chunk_y + b[1] + height, chunk_z + b[2]):
+                    continue
                 _generate_block(chunk, [chunk_x + b[0], chunk_y + b[1] + height, chunk_z + b[2]], self.leaf_name)
         else:
             pass
