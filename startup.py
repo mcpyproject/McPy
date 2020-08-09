19f02ec1323eb4620b5b48d8a6a778944d10471f
beerights@protonmail.com <beerights@protonmail.com>
2020-08-09 12:02:07 -0600

    switch to using `sys.executable`

--- startup.py
+++ startup.py
@@ -148,7 +148,7 @@ if "McPy" not in currentDir:
 else:
     logging.info("McPy found! Running it...")
     if PYTHONINPATH:
-        mcpyProcess = subprocess.Popen("python3.8 McPy/main.py")
+        mcpyProcess = subprocess.Popen("{0} McPy/main.py".format(sys.executable))
     else:
         mcpyProcess = subprocess.Popen("{0} McPy/main.py".format(pythonlocation))
     mcpyProcess.wait()
