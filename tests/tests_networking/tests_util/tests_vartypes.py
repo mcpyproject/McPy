7f79d4bc9a521cc3970e391c31d3fa99ad42a6f8
ntoskrnl4 <22060947+ntoskrnl4@users.noreply.github.com>
2020-08-25 05:51:39 +0000

    Vartype api change; early protocol packets; add Packet ID lookups
    - Moved VarTypes into their own classes for each of them and refactored tests accordingly
    - Added most packets for handshaking/status/login protocol states- just Play packets left (which is ~95% of packets...)
    - Added Packet ID lookups into protocol version's __init__.py
    
    My goal with splitting the protocol versions into their own directories is that
    this should make it easier to update protocol versions and support multiple
    versions at once- you'd copy the protocol version, add support for the new
    version in the (coming) networking.__init__, then go through packets.py and
    make relevant changes to packet formats.
    
    Still coming after this commit is most of the rest of the packets, plus the
    Connection handler class, along with encryption/zlib support, and then chat,
    commands, and the main Networking thread.
    
    This'll be a loooonnnggg project...

--- tests/tests_networking/tests_util/tests_vartypes.py
+++ tests/tests_networking/tests_util/tests_vartypes.py
@@ -2,54 +2,54 @@ import random
 import socket
 import struct
 
-from networking.util.vartypes import *
+from networking.util.vartypes import VarInt, VarLong
 
 
 def test_encode_varint():
     # standard VarInt values
-    assert encode_varint(0) == b"\x00"
-    assert encode_varint(1) == b"\x01"
-    assert encode_varint(2) == b"\x02"
-    assert encode_varint(127) == b"\x7f"
-    assert encode_varint(128) == b"\x80\x01"
-    assert encode_varint(255) == b"\xff\x01"
-    assert encode_varint(2147483647) == b"\xff\xff\xff\xff\x07"
-    assert encode_varint(-1) == b"\xff\xff\xff\xff\x0f"
-    assert encode_varint(-2147483648) == b"\x80\x80\x80\x80\x08"
+    assert VarInt.encode(0) == b"\x00"
+    assert VarInt.encode(1) == b"\x01"
+    assert VarInt.encode(2) == b"\x02"
+    assert VarInt.encode(127) == b"\x7f"
+    assert VarInt.encode(128) == b"\x80\x01"
+    assert VarInt.encode(255) == b"\xff\x01"
+    assert VarInt.encode(2147483647) == b"\xff\xff\xff\xff\x07"
+    assert VarInt.encode(-1) == b"\xff\xff\xff\xff\x0f"
+    assert VarInt.encode(-2147483648) == b"\x80\x80\x80\x80\x08"
     
     # Make sure limits are enforced
-    assert isinstance(encode_varint(2147483648), struct.error)
-    assert isinstance(encode_varint(-2147483649), struct.error)
-    assert isinstance(encode_varint(23489062762390), struct.error)
-    assert isinstance(encode_varint(-12045763498527), struct.error)
-    assert isinstance(encode_varint(324576230948720987534456), struct.error)
-    assert isinstance(encode_varint(-293786529387462938547564), struct.error)
+    assert isinstance(VarInt.encode(2147483648), struct.error)
+    assert isinstance(VarInt.encode(-2147483649), struct.error)
+    assert isinstance(VarInt.encode(23489062762390), struct.error)
+    assert isinstance(VarInt.encode(-12045763498527), struct.error)
+    assert isinstance(VarInt.encode(324576230948720987534456), struct.error)
+    assert isinstance(VarInt.encode(-293786529387462938547564), struct.error)
 
 
 def test_decode_varint_bytes():
     # Test standard VarInt values with bytes object
-    assert decode_varint(b"\x00") == 0, b""
-    assert decode_varint(b"\x01") == 1, b""
-    assert decode_varint(b"\x02") == 2, b""
-    assert decode_varint(b"\x7f") == 127, b""
-    assert decode_varint(b"\x80\x01") == 128, b""
-    assert decode_varint(b"\xff\x01") == 255, b""
-    assert decode_varint(b"\xff\xff\xff\xff\x07") == 2147483647, b""
-    assert decode_varint(b"\xff\xff\xff\xff\x0f") == -1, b""
-    assert decode_varint(b"\x80\x80\x80\x80\x08") == -2147483648, b""
+    assert VarInt.decode(b"\x00") == 0, b""
+    assert VarInt.decode(b"\x01") == 1, b""
+    assert VarInt.decode(b"\x02") == 2, b""
+    assert VarInt.decode(b"\x7f") == 127, b""
+    assert VarInt.decode(b"\x80\x01") == 128, b""
+    assert VarInt.decode(b"\xff\x01") == 255, b""
+    assert VarInt.decode(b"\xff\xff\xff\xff\x07") == 2147483647, b""
+    assert VarInt.decode(b"\xff\xff\xff\xff\x0f") == -1, b""
+    assert VarInt.decode(b"\x80\x80\x80\x80\x08") == -2147483648, b""
     
     # Test invalid inputs
-    assert isinstance(decode_varint(b"\xff\xff\xff\xff\xff\xff"), OverflowError)
-    assert isinstance(decode_varint(b"\x80\x80\x80\x80\x80\x80"), OverflowError)
-    assert isinstance(decode_varint(b"\xff"), TypeError)
-    assert isinstance(decode_varint(b""), IndexError)
+    assert isinstance(VarInt.decode(b"\xff\xff\xff\xff\xff\xff"), OverflowError)
+    assert isinstance(VarInt.decode(b"\x80\x80\x80\x80\x80\x80"), OverflowError)
+    assert isinstance(VarInt.decode(b"\xff"), TypeError)
+    assert isinstance(VarInt.decode(b""), IndexError)
     
     # Test returning rest of bytes[]
-    assert decode_varint(b"\x00OwO") == 0, b"OwO"
-    assert decode_varint(b"\xff\x01Important Packet Here") == 255, b"Important Packet Here"
-    assert decode_varint(b"\xff\xff\xff\xff\x07\x16\xea\xda}\xf8\xa7\x8a\xdc\x83\xa2\xd3^\xb4\x00\x00") == \
+    assert VarInt.decode(b"\x00OwO") == 0, b"OwO"
+    assert VarInt.decode(b"\xff\x01Important Packet Here") == 255, b"Important Packet Here"
+    assert VarInt.decode(b"\xff\xff\xff\xff\x07\x16\xea\xda}\xf8\xa7\x8a\xdc\x83\xa2\xd3^\xb4\x00\x00") == \
            2147483647, b"\x16\xea\xda}\xf8\xa7\x8a\xdc\x83\xa2\xd3^\xb4\x00\x00"
-    assert decode_varint(b"\x80\x80\x80\x80\x081\xa8\xe2\xad\xa4\xad\xad\xac\x1bz\xba\xf2") == \
+    assert VarInt.decode(b"\x80\x80\x80\x80\x081\xa8\xe2\xad\xa4\xad\xad\xac\x1bz\xba\xf2") == \
            -2147483648, b"1\xa8\xe2\xad\xa4\xad\xad\xac\x1bz\xba\xf2"
 
 
@@ -78,68 +78,68 @@ def test_decode_varint_socket():
                 b"\xff\xff\xff\xff\x07"
                 b"\xff\xff\xff\xff\x0f"
                 b"\x80\x80\x80\x80\x08")
-    assert decode_varint(serv_c) == 0
-    assert decode_varint(serv_c) == 1
-    assert decode_varint(serv_c) == 2
-    assert decode_varint(serv_c) == 127
-    assert decode_varint(serv_c) == 128
-    assert decode_varint(serv_c) == 255
-    assert decode_varint(serv_c) == 2147483647
-    assert decode_varint(serv_c) == -1
-    assert decode_varint(serv_c) == -2147483648
+    assert VarInt.decode(serv_c) == 0
+    assert VarInt.decode(serv_c) == 1
+    assert VarInt.decode(serv_c) == 2
+    assert VarInt.decode(serv_c) == 127
+    assert VarInt.decode(serv_c) == 128
+    assert VarInt.decode(serv_c) == 255
+    assert VarInt.decode(serv_c) == 2147483647
+    assert VarInt.decode(serv_c) == -1
+    assert VarInt.decode(serv_c) == -2147483648
     
     # Test too large VarInt, then test EOF
     client.send(b"\xff\xff\xff\xff\xff\xca'\xdf")
-    assert isinstance(decode_varint(serv_c), OverflowError)
+    assert isinstance(VarInt.decode(serv_c), OverflowError)
     assert serv_c.recv(4096) == b"\xca'\xdf"
     client.close()
-    assert isinstance(decode_varint(serv_c), TypeError)
+    assert isinstance(VarInt.decode(serv_c), TypeError)
 
 
 def test_encode_varlong():
     # standard VarLong values
-    assert encode_varlong(0) == b"\x00"
-    assert encode_varlong(1) == b"\x01"
-    assert encode_varlong(2) == b"\x02"
-    assert encode_varlong(127) == b"\x7f"
-    assert encode_varlong(128) == b"\x80\x01"
-    assert encode_varlong(255) == b"\xff\x01"
-    assert encode_varlong(2147483647) == b"\xff\xff\xff\xff\x07"
-    assert encode_varlong(9223372036854775807) == b"\xff\xff\xff\xff\xff\xff\xff\xff\x7f"
-    assert encode_varlong(-1) == b"\xff\xff\xff\xff\xff\xff\xff\xff\xff\x01"
-    assert encode_varlong(-2147483648) == b"\x80\x80\x80\x80\xf8\xff\xff\xff\xff\x01"
-    assert encode_varlong(-9223372036854775808) == b"\x80\x80\x80\x80\x80\x80\x80\x80\x80\x01"
+    assert VarLong.encode(0) == b"\x00"
+    assert VarLong.encode(1) == b"\x01"
+    assert VarLong.encode(2) == b"\x02"
+    assert VarLong.encode(127) == b"\x7f"
+    assert VarLong.encode(128) == b"\x80\x01"
+    assert VarLong.encode(255) == b"\xff\x01"
+    assert VarLong.encode(2147483647) == b"\xff\xff\xff\xff\x07"
+    assert VarLong.encode(9223372036854775807) == b"\xff\xff\xff\xff\xff\xff\xff\xff\x7f"
+    assert VarLong.encode(-1) == b"\xff\xff\xff\xff\xff\xff\xff\xff\xff\x01"
+    assert VarLong.encode(-2147483648) == b"\x80\x80\x80\x80\xf8\xff\xff\xff\xff\x01"
+    assert VarLong.encode(-9223372036854775808) == b"\x80\x80\x80\x80\x80\x80\x80\x80\x80\x01"
     
     # invalid VarLong values
-    assert isinstance(encode_varlong(349876025734624589762384762352), struct.error)
-    assert isinstance(encode_varlong(-104875623985426394857623954873), struct.error)
+    assert isinstance(VarLong.encode(349876025734624589762384762352), struct.error)
+    assert isinstance(VarLong.encode(-104875623985426394857623954873), struct.error)
     
 
 def test_decode_varlong_bytes():
-    assert decode_varint(b"\x00") == 0, b""
-    assert decode_varint(b"\x01") == 1, b""
-    assert decode_varint(b"\x02") == 2, b""
-    assert decode_varint(b"\x7f") == 127, b""
-    assert decode_varint(b"\x80\x01") == 128, b""
-    assert decode_varint(b"\xff\x01") == 255, b""
-    assert decode_varint(b"\xff\xff\xff\xff\x07") == 2147483647, b""
-    assert decode_varint(b"\xff\xff\xff\xff\xff\xff\xff\xff\x7f") == 9223372036854775807, b""
-    assert decode_varint(b"\xff\xff\xff\xff\xff\xff\xff\xff\xff\x01") == -1, b""
-    assert decode_varint(b"\x80\x80\x80\x80\xf8\xff\xff\xff\xff\x01") == -2147483648, b""
-    assert decode_varint(b"\x80\x80\x80\x80\x80\x80\x80\x80\x80\x01") == -9223372036854775808, b""
+    assert VarInt.decode(b"\x00") == 0, b""
+    assert VarInt.decode(b"\x01") == 1, b""
+    assert VarInt.decode(b"\x02") == 2, b""
+    assert VarInt.decode(b"\x7f") == 127, b""
+    assert VarInt.decode(b"\x80\x01") == 128, b""
+    assert VarInt.decode(b"\xff\x01") == 255, b""
+    assert VarInt.decode(b"\xff\xff\xff\xff\x07") == 2147483647, b""
+    assert VarInt.decode(b"\xff\xff\xff\xff\xff\xff\xff\xff\x7f") == 9223372036854775807, b""
+    assert VarInt.decode(b"\xff\xff\xff\xff\xff\xff\xff\xff\xff\x01") == -1, b""
+    assert VarInt.decode(b"\x80\x80\x80\x80\xf8\xff\xff\xff\xff\x01") == -2147483648, b""
+    assert VarInt.decode(b"\x80\x80\x80\x80\x80\x80\x80\x80\x80\x01") == -9223372036854775808, b""
     
     # invalid inputs
-    assert isinstance(decode_varlong(b"\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80"), OverflowError)
-    assert isinstance(decode_varlong(b"\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"), OverflowError)
-    assert isinstance(decode_varlong(b"\xff"), TypeError)
-    assert isinstance(decode_varlong(b""), IndexError)
+    assert isinstance(VarLong.decode(b"\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80"), OverflowError)
+    assert isinstance(VarLong.decode(b"\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"), OverflowError)
+    assert isinstance(VarLong.decode(b"\xff"), TypeError)
+    assert isinstance(VarLong.decode(b""), IndexError)
     
     # Test returning rest of bytes[]
-    assert decode_varlong(b"\x00OwO") == 0, b"OwO"
-    assert decode_varlong(b"\xff\x01Important Packet Here") == 255, b"Important Packet Here"
-    assert decode_varlong(b"\xff\xff\xff\xff\xff\xff\xff\xff\x7f\x16\xea\xda}\xf8\xa7\x8a\xdc\x83\xa2\xd3^\xb4\x00\x00") == \
+    assert VarLong.decode(b"\x00OwO") == 0, b"OwO"
+    assert VarLong.decode(b"\xff\x01Important Packet Here") == 255, b"Important Packet Here"
+    assert VarLong.decode(b"\xff\xff\xff\xff\xff\xff\xff\xff\x7f\x16\xea\xda}\xf8\xa7\x8a\xdc\x83\xa2\xd3^\xb4\x00\x00") == \
            9223372036854775807, b"\x16\xea\xda}\xf8\xa7\x8a\xdc\x83\xa2\xd3^\xb4\x00\x00"
-    assert decode_varlong(b"\x80\x80\x80\x80\x80\x80\x80\x80\x80\x011\xa8\xe2\xad\xa4\xad\xad\xac\x1bz\xba\xf2") == \
+    assert VarLong.decode(b"\x80\x80\x80\x80\x80\x80\x80\x80\x80\x011\xa8\xe2\xad\xa4\xad\xad\xac\x1bz\xba\xf2") == \
            -9223372036854775808, b"1\xa8\xe2\xad\xa4\xad\xad\xac\x1bz\xba\xf2"
 
 
@@ -168,20 +168,20 @@ def test_decode_varlong_socket():
                 b"\xff\xff\xff\xff\xff\xff\xff\xff\xff\x01"
                 b"\x80\x80\x80\x80\xf8\xff\xff\xff\xff\x01"
                 b"\x80\x80\x80\x80\x80\x80\x80\x80\x80\x01")
-    assert decode_varlong(serv_c) == 0
-    assert decode_varlong(serv_c) == 1
-    assert decode_varlong(serv_c) == 2
-    assert decode_varlong(serv_c) == 127
-    assert decode_varlong(serv_c) == 128
-    assert decode_varlong(serv_c) == 255
-    assert decode_varlong(serv_c) == 2147483647
-    assert decode_varlong(serv_c) == 9223372036854775807
-    assert decode_varlong(serv_c) == -1
-    assert decode_varlong(serv_c) == -2147483648
-    assert decode_varlong(serv_c) == -9223372036854775808
+    assert VarLong.decode(serv_c) == 0
+    assert VarLong.decode(serv_c) == 1
+    assert VarLong.decode(serv_c) == 2
+    assert VarLong.decode(serv_c) == 127
+    assert VarLong.decode(serv_c) == 128
+    assert VarLong.decode(serv_c) == 255
+    assert VarLong.decode(serv_c) == 2147483647
+    assert VarLong.decode(serv_c) == 9223372036854775807
+    assert VarLong.decode(serv_c) == -1
+    assert VarLong.decode(serv_c) == -2147483648
+    assert VarLong.decode(serv_c) == -9223372036854775808
     
     client.send(b"\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xca'\xdf")
-    assert isinstance(decode_varlong(serv_c), OverflowError)
+    assert isinstance(VarLong.decode(serv_c), OverflowError)
     assert serv_c.recv(4096) == b"\xca'\xdf"
     client.close()
-    assert isinstance(decode_varlong(serv_c), TypeError)
+    assert isinstance(VarLong.decode(serv_c), TypeError)
