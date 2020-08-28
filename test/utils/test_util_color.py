55de3a4d33f7ca93a1171cb92fbdb21accbf8324
Giacomello Nathan <0ddlyokoOfficial@gmail.com>
2020-08-28 20:37:39 +0200

    [IMP] Change namespace and Color class name
    This commit:
    - Change namespace "minecraft:XXX" to "XXX"
    - Rename namespace id to "namespace_id"
    - Rename class "Color" to "ChatColor"

--- test/utils/test_util_color.py
+++ test/utils/test_util_color.py
@@ -1,13 +1,13 @@
-from classes.utils.Utils import Color
+from classes.utils.Utils import ChatColor
 
 
 class TestColor:
 
     def test_strip_color(self):
-        assert Color.strip_color('Test') == 'Test'
-        assert Color.strip_color('&4Test') == '&4Test'
-        assert Color.strip_color('§4Test') == 'Test'
-        assert Color.strip_color('&4Te§4st') == '&4Test'
-        assert Color.strip_color('&0&1&2&3&4') == '&0&1&2&3&4'
-        assert Color.strip_color('§0§1§2§3§4') == ''
-        assert Color.strip_color('§yTest') == '§yTest'
+        assert ChatColor.strip_color('Test') == 'Test'
+        assert ChatColor.strip_color('&4Test') == '&4Test'
+        assert ChatColor.strip_color('§4Test') == 'Test'
+        assert ChatColor.strip_color('&4Te§4st') == '&4Test'
+        assert ChatColor.strip_color('&0&1&2&3&4') == '&0&1&2&3&4'
+        assert ChatColor.strip_color('§0§1§2§3§4') == ''
+        assert ChatColor.strip_color('§yTest') == '§yTest'
