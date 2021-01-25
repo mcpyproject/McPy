3e48698f10647cb3204e60610616f932996828fc
Geolykt <admin@geolykt.de>
2021-01-25 22:42:50 +0100

    Allow the user to set the log format and make it consistent
    This should solve some minor issues that I encountered with the logging format
    This might be outsourced into a configuration file someday, but in the meantime
    it stays a runtime argument
    
    Resolves #57

--- classes/mcPy/Parser.py
+++ classes/mcPy/Parser.py
@@ -11,12 +11,17 @@ class Parser:
     def initialize_arguments(self):
         self.parser.add_argument("--debug",              # Command line flag to set logging in DEBUG mode
                                  action="store_true",    # Syntactic sugar to say 'default:"false"'
-                                 help="set logging to DEBUG level")
+                                 help="Set logging to DEBUG level")
         self.parser.add_argument("--test",               # Only launch tests
                                  action="store_true",    # Syntactic sugar to say 'default:"false"'
                                  help="Do not launch the server, only launch tests")
+        self.parser.add_argument("--format",             # Command line flag to set the log format
+                                 nargs='?',              # We expect another argument after that
+                                 default="[%(asctime)s - %(levelname)s - %(threadName)s] %(message)s",
+                                 help="Set the logging format to the specified format. (Default: \"%(default)s\")")
 
     def parse_arguments(self):
         self.args = self.parser.parse_args()
         self.debug = self.args.debug
         self.test = self.args.test
+        self.format = self.args.format
