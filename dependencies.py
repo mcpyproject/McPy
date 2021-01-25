0161b7bc807316874d2ef4c44b8bf537d7947ba2
hydrostaticcog <hydrostaticcog@gmail.com>
2021-01-25 15:05:19 -0600

    Remove startup.py in favor of simpler script to install dependencies (#53)
    * Remove startup.py in favor of simpler dependencies.py to install dependencies.
    
    * fixes typo (lack of [] in line 47)
    
    * uses requirements to install dependencies
    
    * fix for review comments (add exception catch for git not being installed. Changed wording in README.md)
    
    * fixes some compatibility issues with Debian/Ubuntu and fixes issues in review
    
    * fix typo
    
    * add fatal warning if dependencies.py is ran without sudoer permissions on ubuntu/debian
    
    * adds exit after failing to install pip (due to lack of sudoer permission
    
    * merge Debian and Ubuntu if statements
    
    * removes unnecessary checks and now requires dependencies.py to be ran with superuser on Debian-based Linux distros. Also fixes typos
    
    * update readme.md for requiring python3.8 or higher
    
    * change wording for superuser requirement
    
    * Remove startup.py in favor of simpler dependencies.py to install dependencies.
    
    fixes typo (lack of [] in line 47)
    
    uses requirements to install dependencies
    
    fix for review comments (add exception catch for git not being installed. Changed wording in README.md)
    
    fixes some compatibility issues with Debian/Ubuntu and fixes issues in review
    
    fix typo
    
    add fatal warning if dependencies.py is ran without sudoer permissions on ubuntu/debian
    
    adds exit after failing to install pip (due to lack of sudoer permission
    
    merge Debian and Ubuntu if statements
    
    removes unnecessary checks and now requires dependencies.py to be ran with superuser on Debian-based Linux distros. Also fixes typos
    
    update readme.md for requiring python3.8 or higher
    
    change wording for superuser requirement

---
+++ dependencies.py
@@ -0,0 +1,48 @@
+import logging
+import sys
+import subprocess
+import json
+
+# Set default logging level to INFO
+logging.basicConfig(level="INFO")
+
+# Check if current python version is 3.8
+if sys.version_info < (3, 8):
+    logging.fatal('McPy needs Python version 3.8.0 or higher to run! Current version is %s.%s.%s' % (
+        sys.version_info.major, sys.version_info.minor, sys.version_info.micro))
+    sys.exit(-4)
+
+# Make sure pip is installed
+logging.info("Making sure pip is installed...")
+try: # Debian and Ubuntu require pip to be installed differently because they disable ensurepip
+    import distro
+    linuxDistro = distro.linux_distribution()
+    if linuxDistro[0] == "Debian" or linuxDistro[0] == "Ubuntu":
+        try:
+            subprocess.check_call(['apt', 'install', 'python3-pip', '-y'])
+        except subprocess.CalledProcessError:
+            logging.fatal("You need to be the superuser to install pip and the dependencies")
+            sys.exit(-4)
+except ModuleNotFoundError: # If the system is not Linux, the module "distro" will not load, causing a ModuleNotFoundError exception and the normal "ensurepip" module will work
+    subprocess.check_call([sys.executable, '-m', 'ensurepip']) # The module ensure pip check if pip is installed and installs it if it isn't
+logging.info("Pip is installed")
+
+# Make sure that dependencies are installed
+logging.info("Installing dependencies...")
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
+try:
+    subprocess.check_call(['git', 'init'])
+    subprocess.check_call(['git', 'submodule', 'init'])
+    subprocess.check_call(['git', 'submodule', 'update'])
+    logging.info("Dependencies installed")
+except subprocess.CalledProcessError:
+    logging.fatal("Git is not installed! Please install it!")
+
+# Finishing up
+with open('releases.json', 'r') as f:
+    release_info = json.load(f)
+
+for releases in release_info:
+    version = releases["mcpyVersion"]
+
+logging.info("McPy version " + version + " is ready. You can run the server with python3 main.py")
