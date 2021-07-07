a2a08dfb9fe6e725e238e2165eaf2ffc3815717f
walksanatora <walkerffo22@hotmail.com>
2021-07-07 13:00:39 -0700

    priority system implemented

--- classes/plugins/load.py
+++ classes/plugins/load.py
@@ -31,40 +31,43 @@ def _getPlugins_() -> Array:
             order[plugin.PRIORITY] = []
         except AttributeError:
             plugin.PRIORITY = 999
-        order[plugin.PRIORITY].append(plugin) 
-    return order
+        order[plugin.PRIORITY].append(plugin)
+    max = 0
+    for p in order:
+        plug = order[p]
+        for i in plug:
+            if i.PRIORITY > max:
+                max = i.PRIORITY
+    ret = []
+    for i in range(max+1):
+        ret.append(None)
+    for p in order:
+        ret[p] = order[p]
+    return ret
 
 def load_plugins():
     event._reset_()
     plugins = _getPlugins_()
     logging.debug(plugins)
-    for priority_num in plugins:
-        priority = plugins[priority_num]
-        if type(priority) != type(event):
-            for plugin_name in priority:
-                imported = plugin_name
+    for index in plugins:
+        if index != None:
+            for imported in index:
                 if inspect.isfunction(getattr(imported, "load", None)):
                     imported.load()
-                    logging.info(f"loaded plugin \"{plugin_name}\"")
+                    logging.info(f"loaded plugin \"{imported.__name__}\"")
                 else:
-                    logging.info(f"couldn't load {plugin_name} because 'load' is not a function or doesen't exist")
-        else:
-            imported = priority
-            if inspect.isfunction(getattr(imported, "load", None)):
-                imported.load()
-                logging.info(f"loaded plugin \"{priority.__name__}\"")
-            else:
-                logging.info(f"couldn't load {priority} because 'load' is not a function or doesen't exist")
-    logging.info("finished loading plugins")
+                    logging.info(f"couldn't load {imported.__name__} because 'load' is not a function or doesen't exist")
+        
+logging.info("finished loading plugins")
 
 #called when server is shutting down
 def unload_plugins():
     plugins = _getPlugins_()
-    for plugin_name in plugins:
-        imported = plugins[plugin_name]
-        if inspect.isfunction(getattr(imported, "unload", None)):
-            imported.unload()
-            logging.info(f"unloaded plugin '{plugin_name}'")
-        else:
-            logging.info(f"unloaded plugin '{plugin_name}' (plugin has no 'unload' function)")
+    for arr in plugins:
+        for imported in arr:
+            if inspect.isfunction(getattr(imported, "unload", None)):
+                imported.unload()
+                logging.info(f"unloaded plugin '{imported.__name__}'")
+            else:
+                logging.info(f"unloaded plugin '{imported.__name__}' (plugin has no 'unload' function)")
     event._reset_()
