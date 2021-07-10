e0e4a77bb9237c31647e94c8f1f2be8080214340
walksanatora <walkerffo22@hotmail.com>
2021-07-10 14:04:11 -0700

    flatten list so easier to itterate over

--- classes/plugins/load.py
+++ classes/plugins/load.py
@@ -16,58 +16,67 @@ def _getPlugins_() -> list:
     def unqualify(name: str) -> str:
         """Return an unqualified name given a qualified module/package `name`."""
         return name.rsplit(".")[-1]
-    plugins = {}
+    
+    #get all ing plugins
+    plugins = []
     for module in pkgutil.walk_packages(exts.__path__, f"{exts.__name__}."):
         plugin_name = unqualify(module.name)
         if plugin_name.startswith("_"):
             continue
-        plugins[plugin_name] = importlib.import_module(module.name)
+        plugins.append(importlib.import_module(module.name))
+    
+    #get a dictionary of the priorities
     order = collections.defaultdict(list)
-    for plugin_name in plugins:
-        plugin = plugins[plugin_name]
+    for plugin in plugins:
+        plugin
         try:
             tmp = order[int(plugin.PRIORITY)]
         except AttributeError:
             plugin.PRIORITY = 999
         order[int(plugin.PRIORITY)].append(plugin)
+
+    #get the highest priority to see how big we have to make the list
     highest_priority = 0
     for p in order:
         plug = order[p]
         for i in plug:
             if int(i.PRIORITY) > highest_priority:
                 highest_priority = int(i.PRIORITY)
-    ret = []
+    list_of_list = []
     for i in range(highest_priority+1):
-        ret.append(None)
+        list_of_list.append(None)
     for p in order:
-        ret[p] = order[p]
+        list_of_list[p] = order[p]
+    
+    #flatten the list of list into a single list
+    ret = []
+    for prior in list_of_list:
+        if prior == None:
+            continue
+        for plugin in prior:
+            ret.append(plugin)
     return ret
 
 def load_plugins():
     event._reset_()
     plugins = _getPlugins_()
     logging.debug(plugins)
-    for index in plugins:
-        if index != None:
-            for imported in index:
-                if inspect.isfunction(getattr(imported, "load", None)):
-                    imported.load()
-                    logging.info(f"loaded plugin \"{imported.__name__}\"")
-                else:
-                    logging.info(f"couldn't load {imported.__name__} because 'load' is not a function or doesen't exist")
-        
-logging.info("finished loading plugins")
+    for imported in plugins:
+        if inspect.isfunction(getattr(imported, "load", None)):
+            imported.load()
+            logging.info(f"loaded plugin \"{imported.__name__}\"")
+        else:
+            logging.info(f"couldn't load {imported.__name__} because 'load' is not a function or doesen't exist")
+    logging.info("finished loading plugins")
 
 #called when server is shutting down
 def unload_plugins():
     plugins = reversed(_getPlugins_())
     logging.debug(plugins)
-    for arr in plugins:
-        if arr != None:
-            for imported in arr:
-                if inspect.isfunction(getattr(imported, "unload", None)):
-                    imported.unload()
-                    logging.info(f"unloaded plugin '{imported.__name__}'")
-                else:
-                    logging.info(f"unloaded plugin '{imported.__name__}' (plugin has no 'unload' function)")
+    for imported in plugins:
+        if inspect.isfunction(getattr(imported, "unload", None)):
+            imported.unload()
+            logging.info(f"unloaded plugin '{imported.__name__}'")
+        else:
+            logging.info(f"unloaded plugin '{imported.__name__}' (plugin has no 'unload' function)")
     event._reset_()
